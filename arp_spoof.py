8ba799c275a88832bd2f8bf2ffa090a2bd4ae722
Kiyotaka_Ayanokoji <75192306+lnznjn@users.noreply.github.com>
2021-10-01 15:40:30 +0800

    Update arp_spoof.py

--- arp_spoof.py
+++ arp_spoof.py
@@ -35,9 +35,9 @@ def body(command):
         print("\033[1;31mhelp\033[0m -- list of all commands\n")
         print("\033[1;31mexit\033[0m -- exit\n")
     
-    elif command.split(" ")[0] == "set":
+    elif command.split(" ")[0] == "set" and command.split("t")[1][0] == ' ':
 
-        if command.split(" ")[1] == "inter":
+        if command.split(" ")[1] == "inter" and command.split(" ")[1].split("r")[1][0] == ' ':
             
             if len(command.split(" ")[2]) == 0:
                 print("\n\033[1;31mError!!!\033[0m\n")
@@ -55,7 +55,7 @@ def body(command):
                 print("\033[1;32m[+]\033[0m interface => {}".format(inter))
                 print("\033[1;32m[+]\033[0m local_mac => {}".format(src_mac))
         
-        elif command.split(" ")[1] == "gatewayIP":
+        elif command.split(" ")[1] == "gatewayIP" and command.split(" ")[1].split("P")[1][0] == ' ':
             if len(command.split(" ")[2]) ==0:
                 print("\n\033[1;31mError!!!\033[0m\n")
                 print("Enter '\033[1;31mhelp\033[0m' for help\n")
@@ -71,7 +71,7 @@ def body(command):
                 
                 print("\033[1;32m[+]\033[0m gatewayIP => {}".format(gIP))
         
-        elif command.split(" ")[1] == "targetIP":
+        elif command.split(" ")[1] == "targetIP" and command.split(" ")[1].split("P")[1][0] == ' ':
             if len(command.split(" ")[2]) ==0:
                 print("\n\033[1;31mError!!!\033[0m\n")
                 print("Enter '\033[1;31mhelp\033[0m' for help\n")
